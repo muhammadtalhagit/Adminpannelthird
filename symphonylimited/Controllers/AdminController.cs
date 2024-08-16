bd49961c7854066b75b9152bfb448bf18b249000
Agha Khalid <khalidali9230@gmail.com>
2024-08-16 12:45:41 +0500

    Now admin pannel embaded start

---
+++ symphonylimited/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace symphonylimited.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddCourses()
+        {
+            return View();
+        }
+
+        public IActionResult EntranceExamminationInfo()
+        {
+            return View();
+        }
+
+        public IActionResult StudentsResult()
+        {
+            return View();
+        }
+
+        public IActionResult AboutInstitute()
+        {
+            return View();
+        }
+
+        public IActionResult FAQs()
+        {
+            return View();
+        }
+
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+
+
+    }
+}
